e79691184fb532a0e5a3006b050b7ab27e9c4b4e
Callum Evans <callumevans@outlook.com>
2020-06-17 22:57:23 +0100

    Just import fontawesome from CDN

--- src/components/social-icons.jsx
+++ src/components/social-icons.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faTwitter, faGithubAlt, faLinkedinIn, faStackOverflow, faPinterest } from "@fortawesome/free-brands-svg-icons";
 import styled from "styled-components";
+import { Helmet } from "react-helmet-async";
 
 const Layout = styled.section`
   display: flex;
@@ -32,19 +31,27 @@ const SocialIcon = styled.a`
   }
 `;
 
-function HoverableIcon({ colour, url, icon }) {
-    return <SocialIcon colour={colour} href={url}><FontAwesomeIcon icon={icon} /></SocialIcon>;
+function HoverableIcon({ colour, url, title, icon }) {
+    return <SocialIcon colour={colour} href={url}><i title={title} className={icon}  /></SocialIcon>;
 }
 
 export default function SocialIcons() {
     return (
-        <Layout>
-            <HoverableIcon aria-label="Facebook" url="https://www.facebook.com/callumevans.1992" colour="#3b5998" icon={faFacebookF} />
-            <HoverableIcon aria-label="Twitter" url="https://twitter.com/callum_evans" colour="#00aced" icon={faTwitter} />
-            <HoverableIcon aria-label="GitHub" url="https://github.com/callumevans" colour="#4e4e4e" icon={faGithubAlt} />
-            <HoverableIcon aria-label="Pinterest" url="https://www.pinterest.co.uk/callumARevans/boards/" colour="#c8232c " icon={faPinterest} />
-            <HoverableIcon aria-label="LinkedIn" url="https://www.linkedin.com/in/callum-evans-2a5420b7" colour="#007bb6" icon={faLinkedinIn} />
-            <HoverableIcon aria-label="Stack Overflow" url="https://stackoverflow.com/users/2850457/callum-evans" colour="#ff9900" icon={faStackOverflow} />
-        </Layout>
+        <>
+            <Helmet
+                script={[
+                    { src: 'https://use.fontawesome.com/releases/v5.0.13/js/brands.js', async: true },
+                    { src: 'https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js', async: true },
+                ]}
+            />
+            <Layout>
+                <HoverableIcon url="https://www.facebook.com/callumevans.1992" colour="#3b5998" title="Facebook" icon="fab fa-facebook-f" />
+                <HoverableIcon url="https://twitter.com/callum_evans" colour="#00aced" title="Twitter" icon="fab fa-twitter" />
+                <HoverableIcon url="https://github.com/callumevans" colour="#4e4e4e" title="GitHub" icon="fab fa-github-alt" />
+                <HoverableIcon url="https://www.pinterest.co.uk/callumARevans/boards/" colour="#c8232c" title="Pinterest" icon="fab fa-pinterest-p" />
+                <HoverableIcon url="https://www.linkedin.com/in/callum-evans-2a5420b7" colour="#007bb6" title="LinkedIn" icon="fab fa-linkedin-in" />
+                <HoverableIcon url="https://stackoverflow.com/users/2850457/callum-evans" colour="#ff9900" title="Stack Overflow" icon="fab fa-stack-overflow" />
+            </Layout>
+        </>
     )
 }

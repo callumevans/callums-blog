bdd5cd0e700e54cd087121d997d70bd0da97241c
Callum Evans <callumevans@outlook.com>
2023-05-06 02:11:46 +0100

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        assets: true,
-    },
-    markdown: {
-        shikiConfig: {
-            theme: 'dark-plus',
-        }
+  site: "https://www.callums.blog",
+  experimental: {
+    assets: true
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'dark-plus'
     }
+  },
+  integrations: [sitemap()]
 });
